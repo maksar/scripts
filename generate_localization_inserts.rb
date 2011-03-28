c5aa8dc396c7cde7a3df77967aaf16b2122c3d30
Alexander Shestakov <Maksar.mail@gmail.com>
2011-03-28 10:52:00 -0500

    Adding query to get missing translations

--- generate_localization_inserts.rb
+++ generate_localization_inserts.rb
@@ -1,13 +1,11 @@
-keys = IO.readlines('keys.txt','').to_s
+#!/usr/bin/env ruby
 
 locales = %w(ar cs da de el EN es es_MX et fi fr fr_CA hu in is it ja ko lt lv ms nb nl pl pt_BR ru sk sv th tr uk vi zh_CN zh_TW)
 
 keys_in_loc = ""
 
 locales.each do |loc|
-	keys_in_loc << keys.gsub(/^(.*)$/) do |m|
-		"if not exists (select keyName from I18N_TRANSLATION where keyName = '#{m}' and locale='#{loc}') begin insert into I18N_TRANSLATION (keyName, locale, comment, value, CreatedTimestamp) values ('#{m}', '#{loc}', null, null, GETDATE()) end;"
-	end
+keys_in_loc <<		"union select t1.* from I18N_TRANSLATION as t1 left join I18N_TRANSLATION as t2 on t1.keyName=t2.keyName and t2.locale = '#{loc}' where t1.locale='en' and t2.value is null and t1.comment like 'CP;%' "
 keys_in_loc << "\n\n"
 end
 
